71993d45c3409d6dc312eaff2faaf1cba63ba303
unknown <racj01@vse.cz>
2020-10-26 20:14:39 +0100

    test signup stránky material-ui

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { route } from 'src/Routes';
 
 export function HomePage() {
   return (
     <div className="appWrapper">
       <h1>Home Page</h1>
-      <p>4IT580 - Project Template</p>
+      <p>
+        Hello! Sign-in or <a href={route.signUp()}>create account</a>
+      </p>
     </div>
   );
 }
