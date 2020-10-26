85f25425b0f3e200dd5b7d1ce3e2b651957f4004
Petr Hovorka <pehovorka@gmail.com>
2020-10-26 17:52:15 +0100

    Material-UI import, basic structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { ScrollToTop } from 'src/utils/ScrollToTop';
+import { CssBaseline } from '@material-ui/core';
+
 import { AuthProvider } from 'src/utils/auth';
 import { EnhancedAppoloProvider } from 'src/utils/apollo';
 import { Routes } from 'src/Routes';
@@ -11,6 +13,7 @@ export function App() {
     <BrowserRouter>
       <AuthProvider>
         <EnhancedAppoloProvider>
+          <CssBaseline />
           <ScrollToTop />
           <Routes />
         </EnhancedAppoloProvider>
---
+++ frontend/src/templates/HomeTemplate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { TopNavigation } from 'src/organisms/';
+
+export function HomeTemplate() {
+  return (
+    <>
+      <TopNavigation />
+    </>
+  );
+}
