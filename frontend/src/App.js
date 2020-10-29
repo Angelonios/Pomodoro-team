3a234712af9b6839447251f85b1db9f8d5dddd78
Petr Hovorka <pehovorka@gmail.com>
2020-10-29 01:04:19 +0100

    Provide pomodoro context to whole app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { ScrollToTop } from 'src/utils/ScrollToTop';
 import { CssBaseline } from '@material-ui/core';
 
 import { AuthProvider } from 'src/utils/auth';
+import { PomodoroProvider } from 'src/utils/PomodoroContext';
 import { EnhancedAppoloProvider } from 'src/utils/apollo';
 import { Routes } from 'src/Routes';
 
@@ -12,11 +13,13 @@ export function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <EnhancedAppoloProvider>
-          <CssBaseline />
-          <ScrollToTop />
-          <Routes />
-        </EnhancedAppoloProvider>
+        <PomodoroProvider>
+          <EnhancedAppoloProvider>
+            <CssBaseline />
+            <ScrollToTop />
+            <Routes />
+          </EnhancedAppoloProvider>
+        </PomodoroProvider>
       </AuthProvider>
     </BrowserRouter>
   );
--- frontend/src/organisms/PomodoroTimer.js
+++ frontend/src/organisms/PomodoroTimer.js
@@ -3,41 +3,37 @@ import { Box, Container, Grid, Paper } from '@material-ui/core';
 import { CircularPomodoroCountdown } from 'src/molecules';
 import { PomodoroTimerButton } from 'src/atoms';
 
-import { PomodoroProvider } from 'src/utils/PomodoroContext';
-
 export function PomodoroTimer() {
   return (
-    <PomodoroProvider>
-      <Container component="main">
-        <Paper elevation={3}>
-          <Box m={4}>
-            <Grid
-              container
-              spacing={10}
-              direction="row"
-              alignItems="center"
-              justify="center"
-            >
-              <Grid item xl={4} lg={4} xs={12} align="center">
-                <CircularPomodoroCountdown />
-              </Grid>
-              <Grid item xl={4} lg={4} xs={12}>
-                <Grid
-                  container
-                  direction="column"
-                  justify="center"
-                  alignItems="center"
-                  spacing={2}
-                >
-                  <Grid item>
-                    <PomodoroTimerButton />
-                  </Grid>
+    <Container component="main">
+      <Paper elevation={3}>
+        <Box m={4}>
+          <Grid
+            container
+            spacing={10}
+            direction="row"
+            alignItems="center"
+            justify="center"
+          >
+            <Grid item xl={4} lg={4} xs={12} align="center">
+              <CircularPomodoroCountdown />
+            </Grid>
+            <Grid item xl={4} lg={4} xs={12}>
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+                spacing={2}
+              >
+                <Grid item>
+                  <PomodoroTimerButton />
                 </Grid>
               </Grid>
             </Grid>
-          </Box>
-        </Paper>
-      </Container>
-    </PomodoroProvider>
+          </Grid>
+        </Box>
+      </Paper>
+    </Container>
   );
 }
