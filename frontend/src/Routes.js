24efd895c2e897c0f818e35f56c9d7555f8d8a93
Jaroslav Mares <jaroslavmares@MacBook-Pro.local>
2020-10-30 17:09:34 +0100

    Initial signIn page from template. No cleaned code.

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -4,10 +4,12 @@ import { Route, Switch } from 'react-router-dom';
 import { HomePage } from 'src/pages/HomePage';
 import { SignUp } from 'src/pages/SignUp';
 import { PageNotFound } from 'src/pages/PageNotFound';
+import { SignIn } from 'src/pages/SignIn';
 
 export const route = {
   home: () => `/`,
   signUp: () => `/auth/signup`,
+  signIn: () => `/signin`,
 };
 
 export function Routes() {
@@ -15,6 +17,7 @@ export function Routes() {
     <Switch>
       <Route path={route.home()} exact component={HomePage} />
       <Route path={route.signUp()} exact component={SignUp} />
+      <Route path={route.signIn()} exact component={SignIn} />
       <Route path="*" component={PageNotFound} />
     </Switch>
   );
