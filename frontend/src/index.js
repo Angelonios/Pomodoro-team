c2eaa063efd3fa385c2b5ef71754640d40258482
Petr Hovorka <pehovorka@gmail.com>
2020-11-06 16:15:49 +0100

    Delete import of CssBaseLine

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from 'src/App';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
 
 const theme = createMuiTheme({
   palette: {
