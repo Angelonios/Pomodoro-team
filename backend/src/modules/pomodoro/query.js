af9ec3ef4b723c932e5e64c0c3fed3eb47e6a33a
Petr Hovorka <pehovorka@gmail.com>
2020-11-14 11:13:20 +0100

    Fix pomodoro query resolver

--- backend/src/modules/pomodoro/query.js
+++ backend/src/modules/pomodoro/query.js
@@ -1,44 +1,55 @@
 export const pomodoro = async (_, { shareId }, { dbConnection }) => {
-  const pomodoro = getPomodoro(shareId);
+  if (shareId !== undefined) {
+    const pomodoro = await getPomodoro({ shareId, dbConnection });
 
-  if (!pomodoro) {
-    console.log('null');
-    return null;
-  }
+    if (!pomodoro) {
+      console.log('null');
+      return null;
+    }
 
-  const currentTime = getDbTime();
+    const currentTime = await getDbTime(dbConnection);
 
-  let secondsSinceStart = 0;
-  let positionInCycle = pomodoro.position_in_cycle;
+    let secondsSinceStart = 0;
+    let positionInCycle = pomodoro.position_in_cycle;
 
-  if (pomodoro.running === 0) {
-    secondsSinceStart = 0;
-  } else {
-    secondsSinceStart =
-      currentTime['CURRENT_TIMESTAMP()'] / 1000 - pomodoro.last_updated / 1000;
-  }
+    if (pomodoro.running === 0) {
+      secondsSinceStart = 0;
+    } else {
+      secondsSinceStart =
+        currentTime['CURRENT_TIMESTAMP()'] / 1000 -
+        pomodoro.last_updated / 1000;
+    }
 
-  const HOUR = 3600;
+    const HOUR = 3600;
 
-  //If timer is idle for more than 10 hours, then restart it
-  if (
-    currentTime['CURRENT_TIMESTAMP()'] / 1000 - pomodoro.last_updated / 1000 >
-    HOUR * 10
-  ) {
-    return { position: 0, secondsSinceStart: 0 };
-  } else {
-    return { position: positionInCycle, secondsSinceStart: secondsSinceStart };
+    //If timer is idle for more than 10 hours, then restart it
+    if (
+      currentTime['CURRENT_TIMESTAMP()'] / 1000 - pomodoro.last_updated / 1000 >
+      HOUR * 10
+    ) {
+      return { position: 0, secondsSinceStart: 0 };
+    } else {
+      console.log({
+        position: positionInCycle,
+        secondsSinceStart: secondsSinceStart,
+      });
+      return {
+        position: positionInCycle,
+        secondsSinceStart: secondsSinceStart,
+      };
+    }
   }
 };
 
-async function getPomodoro(shareId) {
-  const result = await dbConnection.query(`SELECT * FROM pomodoros WHERE share_id = ?`, [
-    shareId
-  ]);
+async function getPomodoro({ shareId, dbConnection }) {
+  const result = await dbConnection.query(
+    `SELECT * FROM pomodoros WHERE share_id = ?`,
+    [shareId],
+  );
   return result[0];
 }
 
-async function getDbTime(){
+async function getDbTime(dbConnection) {
   const result = await dbConnection.query(`SELECT CURRENT_TIMESTAMP()`, []);
   return result[0];
 }
