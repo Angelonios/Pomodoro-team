420db63025f69e2ad1b6b6b65dc869f6eda3ec3c
Jaroslav Mares <jaroslavmares@MacBook-Pro.mshome.net>
2020-11-14 18:08:48 +0100

    zakomentování chyby

--- frontend/src/atoms/MenuItems.js
+++ frontend/src/atoms/MenuItems.js
@@ -2,8 +2,8 @@ import React from 'react';
 import MenuItem from '@material-ui/core/MenuItem';
 
 export function MenuItems({ data }) {
-  console.log({ data });
-  data.map((item) => {
-    return <MenuItem>{item.name}</MenuItem>;
-  });
+  //console.log({ data });
+  //data.map((item) => {
+  //  return <MenuItem>{item.name}</MenuItem>;
+  //});
 }
