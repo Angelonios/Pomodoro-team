1e1276cd0cacbbee50612228906185b10d96fbe1
Jaroslav Mares <jaroslavmares@MacBook-Pro.mshome.net>
2020-11-14 15:09:51 +0100

    doplnění basic queries pro týmy

--- backend/src/modules/team/query.js
+++ backend/src/modules/team/query.js
@@ -32,3 +32,18 @@ export const usersInTeam = async (_, { team_id }, { dbConnection }) => {
     return null;
   }
 };
+
+export const team = async (_, { team_id }, { dbConnection }) => {
+  const team = (
+    await dbConnection.query(`SELECT * FROM teams WHERE team_id = ?`, [team_id])
+  )[0];
+  if (!team) {
+    return null;
+  }
+  return team;
+};
+
+export const teams = async (_, __, { dbConnection }) => {
+  const teams = await dbConnection.query('SELECT * FROM teams');
+  return teams;
+};
