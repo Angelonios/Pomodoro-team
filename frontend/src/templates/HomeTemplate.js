5078e87690700ca1f8343434ac275a9d871e7573
Jaroslav Mares <jaroslavmares@MacBook-Pro.mshome.net>
2020-11-15 15:35:08 +0100

    smazání zapomenuté funkce v hometemplate

--- frontend/src/templates/HomeTemplate.js
+++ frontend/src/templates/HomeTemplate.js
@@ -3,11 +3,6 @@ import { useEffect } from 'react';
 import { PomodoroTimer, TopNavigation } from 'src/organisms';
 
 export function HomeTemplate() {
-  const title = '5:00' + ' ' + 'Break' + ' ' + 'Team Pomodori';
-  useEffect(() => {
-    document.title = title;
-  }, []);
-
   return (
     <>
       <TopNavigation />
