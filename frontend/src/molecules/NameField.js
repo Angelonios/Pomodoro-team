483f99cf52bc2f1234f761156382aa699be3df39
Andrej <feja00@vse.cz>
2020-11-29 19:15:30 +0100

    Namefield and mutation edit

--- frontend/src/molecules/NameField.js
+++ frontend/src/molecules/NameField.js
@@ -2,16 +2,15 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 
-export function NameField({
-  formData,
-  handleChange,
-  formErrors,
-  helperText,
-  data,
-}) {
+export function NameField({ name }) {
   return (
     <Grid item xs={12}>
-      <TextField id="name" label="Display name" variant="outlined" />
+      <TextField
+        id="name"
+        label="Display name"
+        defaultValue={name}
+        variant="outlined"
+      />
     </Grid>
   );
 }
