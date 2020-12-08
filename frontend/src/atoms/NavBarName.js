2de0ffa05c1b4e93c8c876ea377ab44b9a20ec88
Kuba <racj01@vse.cz>
2020-12-08 16:16:37 +0100

    avatar in circle everywhere

--- frontend/src/atoms/NavBarName.js
+++ frontend/src/atoms/NavBarName.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { Box, Tooltip, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Avatar, Box, Tooltip, Typography } from '@material-ui/core';
 import Gravatar from 'react-gravatar';
 
+const useStyles = makeStyles((theme) => ({
+  small: {
+    width: theme.spacing(2),
+    height: theme.spacing(2),
+  },
+}));
+
 export function NavBarName({ email, name }) {
+  const classes = useStyles();
   return (
     <Box style={{ margin: 'auto' }}>
       Logged in as:{' '}
-      <Gravatar
-        email={email}
-        size={20}
-        style={{ verticalAlign: 'sub', border: '1px solid' }}
-      />{' '}
+      <div style={{ display: 'inline-block', verticalAlign: 'top' }}>
+        <Avatar className={classes.small}>
+          <Gravatar
+            email={email}
+            size={20}
+            style={{ verticalAlign: 'sub', border: '1px solid' }}
+          />
+        </Avatar>
+      </div>{' '}
       <Tooltip title={email}>
         <b>{name}</b>
       </Tooltip>
--- frontend/src/atoms/TeamPageName.js
+++ frontend/src/atoms/TeamPageName.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import { Grid, Typography, Tooltip } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Typography, Tooltip, Avatar } from '@material-ui/core';
 import Gravatar from 'react-gravatar';
 
+const useStyles = makeStyles((theme) => ({
+  medium: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+}));
 export function TeamPageName({ email, name }) {
+  const classes = useStyles();
   return (
     <Grid item xs={10} md={4} style={{ textAlign: 'center' }}>
       <div style={{ display: 'inline-flex' }}>
-        <Gravatar
-          email={email}
-          size={45}
-          style={{ verticalAlign: 'sub', padding: '5px' }}
-        />{' '}
+        <Avatar className={classes.medium}>
+          <Gravatar
+            email={email}
+            size={70}
+            style={{ verticalAlign: 'sub', padding: '5px' }}
+          />
+        </Avatar>{' '}
         <Tooltip title={email}>
           <Typography
             component="div"
-            style={{ alignSelf: 'center', wordBreak: 'break-word' }}
+            style={{
+              alignSelf: 'center',
+              wordBreak: 'break-word',
+              marginLeft: '5px',
+            }}
           >
             {' '}
             {name}
