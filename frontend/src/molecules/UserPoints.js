ad684eccd934d22155d4ca25087584870e0b048f
Jaroslav Mares <jaroslavmares@MacBook-Pro.local>
2020-12-10 16:39:53 +0100

    code cleanup

--- frontend/src/molecules/UserPoints.js
+++ frontend/src/molecules/UserPoints.js
@@ -4,7 +4,7 @@ import { Typography } from '@material-ui/core';
 import { gql, useQuery } from '@apollo/client';
 
 export function UserPoints({ user_id }) {
-  const GET_CURRENT_USER_POINTS = gql`
+  const GET_DATA_FOR_USER_POINTS = gql`
     query Points($user_id: Int!) {
       user(user_id: $user_id) {
         used_points
@@ -15,7 +15,7 @@ export function UserPoints({ user_id }) {
     }
   `;
 
-  const { loading, data } = useQuery(GET_CURRENT_USER_POINTS, {
+  const { loading, data } = useQuery(GET_DATA_FOR_USER_POINTS, {
     variables: {
       user_id: user_id,
     },
