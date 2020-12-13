22582f6efbd0481804d7ee0382f40805ad3ea76c
Angelonios <angel8893@email.cz>
2020-12-13 23:29:08 +0100

    Created date helper component, which contains several date functions. Created function for generating 6 months worth of weeks from this day to the past. Created function for preparing empty pages for statistics table Created function for mapping actual work duration to prepared pages Tweaked displaying of statistics table Minor refactoring

--- frontend/src/molecules/StatisticsTable.js
+++ frontend/src/molecules/StatisticsTable.js
@@ -7,92 +7,108 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import TableFooter from '@material-ui/core/TableFooter';
 import TablePagination from '@material-ui/core/TablePagination';
+import { GetWeekNumberFromDate } from '../utils/DateHelper';
+import { Box, Container, Grid, Typography } from '@material-ui/core';
 
-export function StatisticsTable({classes,
-                                rowsPerPage,
-                                rows,
-                                page,
-                                emptyRows,
-                                setRowsPerPage,
-                                handleChangePage,
-                                TablePaginationActions}) {
-
+export function StatisticsTable({
+                                  classes,
+                                  rowsPerPage,
+                                  pages,
+                                  currentPage,
+                                  handleChangePage,
+                                  TablePaginationActions,
+                                }) {
 
   const SECONDS_IN_HOUR = 3600;
   const SECONDS_IN_MINUTE = 60;
 
-  function ISO8601_week_no(dt)
-  {
-    const tdt = new Date(dt.valueOf());
-    const dayn = (dt.getDay() + 6) % 7;
-    tdt.setDate(tdt.getDate() - dayn + 3);
-    const firstThursday = tdt.valueOf();
-    tdt.setMonth(0, 1);
-    if (tdt.getDay() !== 4)
-    {
-      tdt.setMonth(0, 1 + ((4 - tdt.getDay()) + 7) % 7);
+  function formatTime(seconds) {
+    if(seconds === 0){
+      return 'no work done'
     }
-    return 1 + Math.ceil((firstThursday - tdt) / 604800000);
-  }
-
-  function formatTime(seconds){
     const hours = Math.floor(seconds / SECONDS_IN_HOUR);
     const minutes = Math.floor((seconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE);
 
-    const hour_text = (hours === 1) ? hours + " hour " : (hours < 1) ? "" : hours + " hours ";
-    const minute_text = (minutes === 1) ? minutes + " minute " : (minutes < 1) ? "" : minutes + " minutes ";
-    const result = (hour_text.length === 0 && minute_text.length === 0) ? "less than a minute" :  hour_text + minute_text;
+    const hour_text = (hours === 1) ? hours + ' hour ' : (hours < 1) ? '' : hours + ' hours ';
+    const minute_text = (minutes === 1) ? minutes + ' minute ' : (minutes < 1) ? '' : minutes + ' minutes ';
+    const result = (hour_text.length === 0 && minute_text.length === 0) ? 'less than a minute' : hour_text + minute_text;
 
     return result;
   }
+debugger;
+  const week = (pages.length !== 0)
+    ? pages[currentPage].sort((d1, d2) => d1 > d2 ? 1 : -1)
+    : [];
+  const weekNumber = GetWeekNumberFromDate((pages.length !== 0) ? week[0].date : new Date());
 
-  // const distinctBy = (prop, arr) => [...new Set(arr.map(o => o[prop]))];
-  // const distinct_weeks = distinctBy('week_no', rows);
-  const max_week_no = ISO8601_week_no(new Date());
-  const current_rows = rows.filter(r => r.week_no === (max_week_no - page));
 
   return (
-    <TableContainer component={Paper}>
+    <Container component="main">
+      <Paper elevation={3}>
+        <Box p={3}>
+          <Grid item xs={12} style={{ textAlign: 'center' }}>
+            <Typography align={'center'} variant={'h3'}>
+              Week {weekNumber}.
+            </Typography>
+          </Grid>
+          <TableContainer component={Paper}>
 
-      <Table className={classes.table} aria-label="custom pagination table">
-        <TableBody>
-          {rows.filter(r => r.week_no === (max_week_no - page))
-            .map((row) => (
-            <TableRow key={row.finished_at}>
-              <TableCell component="th" scope="row">
-                {row.finished_at}
-              </TableCell>
-              <TableCell style={{ width: 160 }} align="right">
-                {formatTime(row.duration)}
-              </TableCell>
-            </TableRow>
-          ))}
+            <Table className={classes.table} aria-label="custom pagination table">
+              <TableBody>
+                {(pages.length !== 0) ?
+                  week.map(day => (
+                  <TableRow key={day.date.toLocaleString('en-us', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                    weekday: 'long',
+                  })}>
 
-          {/*{emptyRows > 0 && (*/}
-          {/*  <TableRow style={{ height: 53 * emptyRows }}>*/}
-          {/*    <TableCell colSpan={6} />*/}
-          {/*  </TableRow>*/}
-          {/*)}*/}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-              colSpan={3}
-              count={rows.length}
-              rowsPerPage={rowsPerPage}
-              labelDisplayedRows={() => {''}}
-              page={page}
-              SelectProps={{
-                inputProps: { 'aria-label': 'rows per page' },
-                native: true,
-              }}
-              rowsPerPageOptions={[]}
-              onChangePage={handleChangePage}
-              ActionsComponent={TablePaginationActions}
-            />
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
+                    <TableCell component="th" scope="row">
+                      {day.date.toLocaleString('en-us', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric',
+                        weekday: 'long',
+                      })}
+                    </TableCell>
+
+                    <TableCell style={{ width: 160 }} align="right">
+                      {formatTime(day.work)}
+                    </TableCell>
+
+                  </TableRow>
+                ))
+                : <TableRow component="th" scope="row">
+                    <TableCell style={{ width: 160 }} align="right">
+                      No data available.
+                    </TableCell>
+                  </TableRow>}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TablePagination
+                    colSpan={3}
+                    count={pages.length}
+                    rowsPerPage={rowsPerPage}
+                    labelDisplayedRows={() => {
+                      '';
+                    }}
+                    page={currentPage}
+                    SelectProps={{
+                      inputProps: { 'aria-label': 'rows per page' },
+                      native: true,
+                    }}
+                    rowsPerPageOptions={[]}
+                    onChangePage={handleChangePage}
+                    ActionsComponent={TablePaginationActions}
+                  />
+                </TableRow>
+              </TableFooter>
+            </Table>
+          </TableContainer>
+        </Box>
+      </Paper>
+    </Container>
   );
 }
--- frontend/src/organisms/PomodoroStatistics.js
+++ frontend/src/organisms/PomodoroStatistics.js
@@ -1,113 +1,77 @@
 import React from 'react';
-import { gql, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import { useAuth } from '../utils/auth';
 import { TablePaginationActions } from 'src/molecules/TablePaginationActions';
 import { StatisticsTable } from 'src/molecules/StatisticsTable';
-import { Box, Container, Paper } from '@material-ui/core';
-
-const POMODORO_STATISTICS = gql`
-  query pomodoroStatistics($user_id: Int!) {
-    pomodoroStatistics(user_id: $user_id) {
-        finished_at,
-        duration,
-    }
-  }
-`;
+import { Container } from '@material-ui/core';
+import { POMODORO_STATISTICS } from 'src/utils/serverSync';
+import { useAuth } from '../utils/auth';
+import { PrepareWeeks } from '../utils/DateHelper';
 
 export function PomodoroStatistics() {
+  const ROWS_PER_PAGE = 7;
   const auth = useAuth();
-  // if (!user) {
-  //   return <HomePage />;
-  // }
+  const [currentPage, setCurrentPage] = React.useState(0);
+
+  const { loading, data } = useQuery(POMODORO_STATISTICS, {
+    variables: { user_id: auth.user.user_id },
+  });
   const tableStyle = makeStyles({
     table: {
       minWidth: 500,
     },
   });
 
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(7);
+  function prepareData(data) {
+    const workDurations = data.pomodoroStatistics;
+    const preparedWeeks = PrepareWeeks();
+    const preparedPages = preparedWeeks.map(week =>
+      week.map(day => {
+        return ({
+          date: day,
+          work: 0,
+        });
+      }),
+    );
 
-
-  const { loading, data } = useQuery(POMODORO_STATISTICS, {
-    variables: { user_id: auth.user.user_id },
-  });
-
-  function ISO8601_week_no(dt) {
-    const tdt = new Date(dt.valueOf());
-    const dayn = (dt.getDay() + 6) % 7;
-    tdt.setDate(tdt.getDate() - dayn + 3);
-    const firstThursday = tdt.valueOf();
-    tdt.setMonth(0, 1);
-    if (tdt.getDay() !== 4) {
-      tdt.setMonth(0, 1 + ((4 - tdt.getDay()) + 7) % 7);
-    }
-    return 1 + Math.ceil((firstThursday - tdt) / 604800000);
+    const finishedPages = mapWorkDatesToPages(workDurations, preparedPages)
+    return finishedPages;
   }
 
-  function parseData(data) {
-    const sorted = data.pomodoroStatistics
-      .map(s => ({
-        finished_at: parseInt(s.finished_at),
-        duration: s.duration,
-      }))
-      .sort((s1, s2) => (
-        s1.finished_at < s2.finished_at ? -1 : 1
-      ))
-      .map(s => ({
-        finished_at: (new Date(s.finished_at)).toLocaleString('en-us', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-          weekday: 'long',
-        }),
-        duration: s.duration,
-      }));
-
-    const distinctBy = (prop, arr) => [...new Set(arr.map(o => o[prop]))];
-    const distinct_dates = distinctBy('finished_at', sorted);
-
-    const aggregated = distinct_dates.map(d => {
-      let sum = 0;
-      sorted.forEach(s => {
-        if (s.finished_at === d) {
-          for (let property in s) {
-            if (property === 'finished_at') {
-              sum += s.duration;
-            }
+  function mapWorkDatesToPages(workDurations, preparedPages){
+    for (let i = 0; i < preparedPages.length; i++){
+      const week = preparedPages[i];
+      for (let j = 0; j < week.length; j++){
+        const day = week[j];
+        for (let k = 0; k < workDurations.length; k++){
+          const workDay = workDurations[k];
+          const workDayDate = new Date(parseInt(workDay.finished_at));
+          if(day.date.getTime() === workDayDate.getTime()){
+            day.work = workDay.duration;
+            console.log("mapped work to day: " + day);
           }
         }
-      });
-      return ({
-        week_no: ISO8601_week_no(new Date(d)),
-        finished_at: d,
-        duration: sum,
-      });
-    });
-
-    return aggregated;
+      }
+    }
+    return preparedPages;
   }
 
-  const rows = (loading) ? [] : parseData(data);
-  // debugger;
-  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+  let pages = (loading) ? [] : prepareData(data);
 
   const handleChangePage = (event, newPage) => {
-    setPage(newPage);
+    setCurrentPage(newPage);
   };
 
   const paginationData = {
-    count: rows.length,
-    page: page,
-    rowsPerPage: rowsPerPage,
+    count: pages.length,
+    page: currentPage,
+    rowsPerPage: ROWS_PER_PAGE,
     onChangePage: handleChangePage,
   };
 
   const tableActions = () => TablePaginationActions(paginationData);
 
-  /* Typechecking setting for table actions */
   tableActions.propTypes = {
     count: PropTypes.number.isRequired,
     onChangePage: PropTypes.func.isRequired,
@@ -115,15 +79,12 @@ export function PomodoroStatistics() {
     rowsPerPage: PropTypes.number.isRequired,
   };
 
-  // debugger;
   return <>
     <Container component="main">
       <StatisticsTable classes={tableStyle()}
-                       rowsPerPage={rowsPerPage}
-                       rows={rows}
-                       page={page}
-                       emptyRows={emptyRows}
-                       setRowsPerPage={setRowsPerPage}
+                       rowsPerPage={ROWS_PER_PAGE}
+                       pages={pages}
+                       currentPage={currentPage}
                        handleChangePage={handleChangePage}
                        TablePaginationActions={tableActions}
       />
